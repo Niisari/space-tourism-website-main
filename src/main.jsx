7e15c38fe45ecc689ff7f6195b2229045c44cf1e
Yushi <yushari@seznam.cz>
2026-04-18 15:06:36 +0200

    Added BrowserRouter for page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import "./index.css";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
 );
