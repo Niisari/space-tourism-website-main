c4d41c169114d29c7841492c1a61d250add4b63d
Yushi <yushari@seznam.cz>
2026-05-05 11:23:18 +0200

    wrapped the destination page in AnimationWrapper to add a fadein animation

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,5 +1,6 @@
 import "./Destination.css";
 import data from "../../data/data.json";
+import { AnimationWrapper } from "../../components/AnimationWrapper";
 import { useState } from "react";
 
 export function Destination() {
@@ -10,54 +11,56 @@ export function Destination() {
   );
 
   return (
-    <section className="Destination__container">
-      <div className="Destination__subtitle">
-        <span className="subtitle__num">01</span>
-        <span className="subtitle__text">PICK YOUR DESTINATION</span>
-      </div>
+    <AnimationWrapper>
+      <section className="Destination__container">
+        <div className="Destination__subtitle">
+          <span className="subtitle__num">01</span>
+          <span className="subtitle__text">PICK YOUR DESTINATION</span>
+        </div>
 
-      <div className="Destination__img">
-        <img
-          src={selectedDestination.images.png}
-          alt={selectedDestination.name}
-        />
-      </div>
+        <div className="Destination__img">
+          <img
+            src={selectedDestination.images.png}
+            alt={selectedDestination.name}
+          />
+        </div>
 
-      <div className="Destination__btn--content">
-        <ul className="Destination__btn--list">
-          {destinations.map((destination) => (
-            <li
-              key={destination.name}
-              className={
-                destination.name === currDestination
-                  ? "Destination__btn--active"
-                  : ""
-              }
-              onClick={() => setCurrDestination(destination.name)}
-            >
-              {destination.name}
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div className="Destination__btn--content">
+          <ul className="Destination__btn--list">
+            {destinations.map((destination) => (
+              <li
+                key={destination.name}
+                className={
+                  destination.name === currDestination
+                    ? "Destination__btn--active"
+                    : ""
+                }
+                onClick={() => setCurrDestination(destination.name)}
+              >
+                {destination.name}
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className="Destination__text--content">
-        <h2 className="Destination__title">{selectedDestination.name}</h2>
-        <p className="Destination__description">
-          {selectedDestination.description}
-        </p>
+        <div className="Destination__text--content">
+          <h2 className="Destination__title">{selectedDestination.name}</h2>
+          <p className="Destination__description">
+            {selectedDestination.description}
+          </p>
 
-        <div className="Destination__avg--content">
-          <div className="Destination__avg--distance">
-            <h3 className="avg__title">Avg. distance</h3>
-            <p className="avg__text">{selectedDestination.distance}</p>
-          </div>
-          <div className="Destination__est--travel">
-            <h3 className="avg__title">Est. travel time</h3>
-            <p className="avg__text">{selectedDestination.travel}</p>
+          <div className="Destination__avg--content">
+            <div className="Destination__avg--distance">
+              <h3 className="avg__title">Avg. distance</h3>
+              <p className="avg__text">{selectedDestination.distance}</p>
+            </div>
+            <div className="Destination__est--travel">
+              <h3 className="avg__title">Est. travel time</h3>
+              <p className="avg__text">{selectedDestination.travel}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </AnimationWrapper>
   );
 }
