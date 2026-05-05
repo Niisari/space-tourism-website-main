b32acc8fc036e635df8c268ec472a65d6797e6bd
Yushi <yushari@seznam.cz>
2026-05-05 09:15:05 +0200

    Imported AnimationWrapper to apply fadein animation to the page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,23 @@
 import "./HomePage.css";
+import { AnimationWrapper } from "../../components/AnimationWrapper";
 export function HomePage() {
   return (
-    <div className="HomePage__container">
-      <div className="HomePage__text--content">
-        <span className="HomePage__subtitle">SO, YOU WANT TO TRAVEL TO</span>
-        <h1 className="HomePage__title">SPACE</h1>
-        <p className="HomePage__paragraph">
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
-      </div>
-      <div className="HomePage__btn--wrapper">
-        <button className="HomePage__btn">EXPLORE</button>
-      </div>
-    </div>
+    <AnimationWrapper>
+      <section className="HomePage__container">
+        <div className="HomePage__text--content">
+          <span className="HomePage__subtitle">SO, YOU WANT TO TRAVEL TO</span>
+          <h1 className="HomePage__title">SPACE</h1>
+          <p className="HomePage__paragraph">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="HomePage__btn--wrapper">
+          <button className="HomePage__btn">EXPLORE</button>
+        </div>
+      </section>
+    </AnimationWrapper>
   );
 }
