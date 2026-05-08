096b44c3b929c736c73fbee886759ac68067818a
Yushi <yushari@seznam.cz>
2026-05-08 11:41:05 +0200

    Added wrapper around the homepage for background

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,21 +3,25 @@ import { AnimationWrapper } from "../../components/AnimationWrapper";
 export function HomePage() {
   return (
     <AnimationWrapper>
-      <section className="HomePage__container">
-        <div className="HomePage__text--content">
-          <span className="HomePage__subtitle">SO, YOU WANT TO TRAVEL TO</span>
-          <h1 className="HomePage__title">SPACE</h1>
-          <p className="HomePage__paragraph">
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </div>
-        <div className="HomePage__btn--wrapper">
-          <button className="HomePage__btn">EXPLORE</button>
-        </div>
-      </section>
+      <div className="HomePage__wrapper">
+        <section className="HomePage__container">
+          <div className="HomePage__text--content">
+            <span className="HomePage__subtitle">
+              SO, YOU WANT TO TRAVEL TO
+            </span>
+            <h1 className="HomePage__title">SPACE</h1>
+            <p className="HomePage__paragraph">
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className="HomePage__btn--wrapper">
+            <button className="HomePage__btn">EXPLORE</button>
+          </div>
+        </section>
+      </div>
     </AnimationWrapper>
   );
 }
