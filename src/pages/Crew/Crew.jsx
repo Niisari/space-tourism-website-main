08d8fbc3fa7f40a87e08340a6d521ad1f7a441ed
Yushi <yushari@seznam.cz>
2026-05-18 12:12:07 +0200

    Imported and added AnimationWrapper component for slight fade in page transition

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AnimationWrapper } from "../../components/AnimationWrapper";
 import data from "../../data/data.json";
 import "./Crew.css";
 
@@ -9,43 +10,45 @@ export function Crew() {
   const selectedCrewMember = crewMembers[currCrew];
 
   return (
-    <div className="Crew__wrapper">
-      <div className="Crew__container">
-        <div className="Crew__subtitle">
-          <span className="subtitle__num">02</span>
-          <span className="subtitle__text">MEET YOUR CREW</span>
-        </div>
+    <AnimationWrapper>
+      <div className="Crew__wrapper">
+        <section className="Crew__container">
+          <div className="Crew__subtitle">
+            <span className="subtitle__num">02</span>
+            <span className="subtitle__text">MEET YOUR CREW</span>
+          </div>
 
-        <div className="Crew__content">
-          <div className="Crew__text--content">
-            <div className="Crew__text--wrapper">
-              <h2 className="Crew__title">{selectedCrewMember.role}</h2>
-              <h1 className="Crew__name">{selectedCrewMember.name}</h1>
-              <p className="Crew__paragraph">{selectedCrewMember.bio}</p>
+          <div className="Crew__content">
+            <div className="Crew__text--content">
+              <div className="Crew__text--wrapper">
+                <h2 className="Crew__title">{selectedCrewMember.role}</h2>
+                <h1 className="Crew__name">{selectedCrewMember.name}</h1>
+                <p className="Crew__paragraph">{selectedCrewMember.bio}</p>
+              </div>
+              <div className="Crew__btn--wrapper">
+                <div className="Crew__btn--list">
+                  {crewMembers.map((crewMember, index) => (
+                    <button
+                      key={crewMember.name}
+                      className={`dot ${index === currCrew ? "active" : ""}`}
+                      onClick={() => setCurrCrew(index)}
+                    ></button>
+                  ))}
+                </div>
+              </div>
             </div>
-            <div className="Crew__btn--wrapper">
-              <div className="Crew__btn--list">
-                {crewMembers.map((crewMember, index) => (
-                  <button
-                    key={crewMember.name}
-                    className={`dot ${index === currCrew ? "active" : ""}`}
-                    onClick={() => setCurrCrew(index)}
-                  ></button>
-                ))}
+
+            <div className="Crew__img--wrapper">
+              <div className="Crew__img">
+                <img
+                  src={selectedCrewMember.images.png}
+                  alt={selectedCrewMember.name}
+                />
               </div>
             </div>
           </div>
-
-          <div className="Crew__img--wrapper">
-            <div className="Crew__img">
-              <img
-                src={selectedCrewMember.images.png}
-                alt={selectedCrewMember.name}
-              />
-            </div>
-          </div>
-        </div>
+        </section>
       </div>
-    </div>
+    </AnimationWrapper>
   );
 }
