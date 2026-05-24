ee20bc53b108bd7e322a8dad2fb0f86edb6e47f7
Yushi <yushari@seznam.cz>
2026-05-24 09:29:16 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./styles/Global.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/space-tourism-website-main">
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
